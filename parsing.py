7cb8cbdd706d3ef827dab46914945b6dcbdfc597
george <georgiy.grigorev@reface.ai>
2022-04-19 19:07:19 +0400

    raise prices and change links

--- parsing.py
+++ parsing.py
@@ -7,8 +7,8 @@ from zoopla_scraper import parse_weblink, get_apartments
 
 logger = logging.getLogger(__name__)
 
-DAILY_RIGHTMOVE_URL = "https://www.rightmove.co.uk/property-to-rent/find.html?locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22y%7CpyH%60aSjF_xAR%7DfAtD%7D%7DAk%5Dih%40kj%40sHugAf%60%40kx%40%60e%40_e%40jmAjI~tBuG~r%40~A%60_B~Vhw%40h%60%40re%40vlA%60e%40j_Al%5BjUfBkDimAi%40sbA~AifAhDkw%40%22%7D&maxBedrooms=2&minBedrooms=1&maxPrice=2000&minPrice=1500&radius=0.25&propertyTypes=flat&primaryDisplayPropertyType=flats&maxDaysSinceAdded=1&mustHave=&dontShow=student%2Cretirement%2ChouseShare&furnishTypes=&letType=longTerm&keywords="
-DAILY_ZOOPLA_URL = r"https://www.zoopla.co.uk/to-rent/flats/london/?added=24_hours&beds_max=2&beds_min=1&include_shared_accommodation=false&page_size=25&polyenc=ujqyHzkS`UolEu^imAmfAo\uoAre@_l@`pDhNvuC~_@bfCpu@hiAzl@fF`bAc^aFeuF&price_frequency=per_month&price_max=2000&price_min=1500&view_type=list&q=London&radius=0.25&results_sort=newest_listings&search_source=refine"
+DAILY_RIGHTMOVE_URL = r"https://www.rightmove.co.uk/property-to-rent/find.html?minBedrooms=1&keywords=&channel=RENT&index=0&letType=long_term&maxBedrooms=2&sortType=2&minPrice=1750&viewType=LIST&maxPrice=2250&radius=0.25&maxDaysSinceAdded=1&locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22%7DbvyH%7ClYi%60%40se%40_Wiw%40_Ba_BtG_s%40ds%40wXeVknAkh%40giBtn%40uvAjeA_g%40j%60Ase%40t_%40fF%60bAhVdLb%7DAq%60%40nu%40kRtkA%7DD%7CfAkF%7EwAiDjw%40_BhfAh%40rbAjDhmAkUgBk_Am%5BwlAae%40%22%7D"
+DAILY_ZOOPLA_URL = r"https://www.zoopla.co.uk/to-rent/flats/london/?added=24_hours&beds_max=2&beds_min=1&include_shared_accommodation=false&page_size=25&polyenc=ujqyHzkS`YgkDf{@yjBgnAchBas@yNat@nQkTnhAor@jb@yGdkAnWp|Boh@`Ohe@`sFpu@hiAzl@fF`bAc^aFeuF&price_frequency=per_month&price_max=2250&price_min=1750&view_type=list&q=London&radius=0&results_sort=newest_listings&search_source=refine"
 
 
 def get_zoopla_feed(seen_links):
